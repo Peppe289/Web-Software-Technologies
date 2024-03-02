cf3c2efdd3afa9372cc1ed6f00136383027e63da
Peppe289 <gsperanza204@protonmail.com>
2024-03-02 11:34:04 +0100

    clear yettodo

--- src/main/java/org/example/primaapp/Yettodo.java
+++ src/main/java/org/example/primaapp/Yettodo.java
@@ -7,20 +7,11 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "do-nothing", value = "/do-nothing")
 public class Yettodo extends HttpServlet {
-    private String message;
-
     public void init() {
-
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + "Pagina bianca obv" + "</h1>");
-        out.println("</body></html>");
     }
 
     public void destroy() {
